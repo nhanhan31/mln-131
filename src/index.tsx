8b3f9448c9105f47ed1971edb771756da1f2d206
Nhannhgit config user.email <nhantdse161743@fpt.edu.vn>
2025-11-10 09:32:04 +0700

    Fix: Remove unused variables and imports for Vercel build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Analytics } from "@vercel/analytics/react"
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -8,5 +9,6 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
